d2a68d258350a8dea53c80349cea42a6364b88a9
Joaquin Noe <joako.n96@gmail.com>
2017-12-07 00:04:39 -0300

    caching all assets

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -8,7 +8,6 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
     event.preventDefault();
     deferredPrompt = event;
     return false;
